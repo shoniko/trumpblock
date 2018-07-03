edc07ff4e0672f4545b3e6e26f42cc05e35c4efa
Oleksandr Paraska <shoniko@gmail.com>
2018-07-03 09:54:24 +0300

    Implement POC

--- background.js
+++ background.js
@@ -1,29 +1,69 @@
-const MODEL_URL = '/models'
-const minConfidence = 0.6
+(async function(){
+  const MODEL_URL = '/models'
+  const minConfidence = 0.5
+  const substituteImage = new Image();
+  let targetDescriptor = null;
 
-async function initModels() {
-  await faceapi.loadModels(MODEL_URL);
-};
+  async function initModels() {
+    await faceapi.loadModels(MODEL_URL);
+  };
 
-initModels();
-
-chrome.runtime.onMessage.addListener(
-  async function (request, sender, sendResponse) {
-    console.log(sender.tab ?
-      "from a content script:" + sender.tab.url :
-      "from the extension");
-    if (typeof request.source != "undefined") {
-      sendResponse({ result: "processing" });
-
-      let input = document.createElement("img");
-      input.src = request.source;
-
-      const fullFaceDescriptions = await faceapi.allFaces(input, minConfidence);
-
-      input.remove();
-
-      chrome.tabs.sendMessage(sender.tab.id, {result: fullFaceDescriptions}, function(response) {
-        console.log(response);
-      });
+  async function loadTargetPersona() {
+    let input = new Image()
+    input.onload = async function() {
+      targetDescriptor = await faceapi.allFaces(
+        this, minConfidence
+      );
+      targetDescriptor = targetDescriptor[0].descriptor;
     }
-  });
+    input.src = "icons/detailed/Donald_Trump_official_portrait.jpg"
+  }
+
+  function loadSubstituteImage() {
+    substituteImage.src = "icons/detailed/Carrot-128a.png";
+  }
+
+  await initModels();
+  loadSubstituteImage();
+  await loadTargetPersona();
+
+  chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+      if (!sender.tab) {
+        return;
+      }
+      if (typeof request.source != "undefined") {
+        sendResponse({ result: "processing" });
+
+        let input = new Image()
+        input.onload = async function() {
+          const fullFaceDescriptions = await faceapi.allFaces(this, minConfidence);
+          
+          let canvas = document.createElement("canvas");
+          canvas.width = this.width;
+          canvas.height = this.height;
+          let ctx = canvas.getContext("2d");
+          ctx.drawImage(this, 0, 0);
+          fullFaceDescriptions.forEach(face => {
+            const distance = faceapi.euclideanDistance(
+              targetDescriptor, face.descriptor
+            );
+            if (distance < minConfidence) {
+              ctx.drawImage(substituteImage, 
+                face.detection._box.x, face.detection._box.y-30, 
+                face.detection._box.width, face.detection._box.height+30);
+            }
+          });
+          let fixedImageBase64 = canvas.toDataURL("image/png");
+          chrome.tabs.sendMessage(sender.tab.id, {
+            url: this.currentSrc, 
+            data: fixedImageBase64
+          }, function(response) {
+            console.log(response);
+          });
+          this.remove();
+        }
+        input.src = request.source;
+      }
+    });
+})();
--- contentscript.js
+++ contentscript.js
@@ -1,31 +1,28 @@
-function imageToBase64String(image) {
-  try {
-    let canvas = document.createElement("canvas");
-    canvas.width = image.width;
-    canvas.height = image.height;
-    let ctx = canvas.getContext("2d");
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    
-    return canvas.toDataURL("image/png");
-  }
-  catch(e) {
-    return "";
-  }
+function onLoad() {
+  Array.from(document.images).forEach(image => {
+    if ((image.height > 100) && (image.width > 100)) {
+      chrome.runtime.sendMessage({ source: image.currentSrc },
+        function (response) {
+          console.log(response);
+      });
+    }
+  });
 };
 
-Array.from(document.images).forEach(image => {
-  if ((image.height > 100) && (image.width > 100)) {
-    image.crossOrigin = "Anonymous";
-    chrome.runtime.sendMessage({ source: imageToBase64String(image), imageId: image },
-      function (response) {
-        console.log(response);
-    });
-  }
-});
+window.addEventListener("load", onLoad, false);
+
 
 chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    console.log(sender.tab ?
-      "from a content script:" + sender.tab.url :
-      "from the extension");
-    });
+  function (result, sender, sendResponse) {
+    
+    if (!sender.tab) {
+      Array.from(document.images).forEach(image => {
+        if ((image.height > 100) && (image.width > 100)) {
+          if (image.currentSrc == result.url)
+          {
+            image.src = result.data;
+          }
+        }
+      });
+    }
+  });
